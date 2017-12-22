de96d05fdf3360dcb6c4982255a22ce4c8225f5c
Mark Probst <mark.probst@gmail.com>
2017-12-21 17:02:03 -0800

    WIP: Generate with the correct language

--- VSCode/src/extension.ts
+++ VSCode/src/extension.ts
@@ -5,11 +5,17 @@ import { Range } from "vscode";
 import { paste } from "copy-paste";
 import { quicktype } from "quicktype";
 
+const supportedLanguages: string[] = [ "cpp", "csharp", "go", "typescript", "swift", "elm", "java" ];
+
 function pasteJSONAsCode(editor: vscode.TextEditor): void {
+    let language = editor.document.languageId;
+    if (supportedLanguages.indexOf(language) < 0) {
+        language = "types";
+    }
     paste((err, content) => {
         if (err) return;
         quicktype({
-            lang: "types",
+            lang: language,
             sources: [{name: "TopLevel", samples: [content]}]
         }).then(result => {
             const text = result.lines.join("\n");
